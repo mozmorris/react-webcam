995aa77da20c72d7ab6c2e01075b6664025c6110
KaranJariwala <karanjariwala@gmail.com>
2019-06-07 12:53:41 +0530

    fix bracket
    fix bracket

--- src/react-webcam.js
+++ src/react-webcam.js
@@ -247,7 +247,8 @@ export default class Webcam extends Component {
           .getWebcam(constraints)
           .then(handleUserMediaSuccess)
           .catch(handleUserMediaFailure);
-      } 
+      }
+    };  
 
     if ('mediaDevices' in navigator) {
       sourceSelected(props.audioConstraints, props.videoConstraints);
